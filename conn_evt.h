4c32b2462b5dcc2903e44a19c57b1347823cca83
Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>
2014-10-10 13:58:38 +0200

    Added conn_evt module, takes care of LL state and synchronization

---
+++ conn_evt.h
@@ -0,0 +1,11 @@
+#ifndef _CONN_EVT_H__
+#define _CONN_EVT_H__
+#include <stdint.h>
+
+void conn_evt_init(void);
+
+void conn_evt_start(void);
+
+void conn_evt_reset(void);
+
+#endif /* _CONN_EVT_H__ */
