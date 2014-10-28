c47d0f8595147c60dfc1ed4ad6c3ba3b66fced0a
Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>
2014-10-28 18:15:24 +0100

    Started API implementation

--- rebroadcast.c
+++ rebroadcast.c
@@ -1,2 +1,145 @@
-#include "trickle.h"
+#include "rebroadcast.h"
 #include "trickle_common.h"
+#include "rbc_database.h"
+
+#include "nrf_error.h"
+
+#include <string.h>
+
+
+/*****************************************************************************
+* Static globals
+*****************************************************************************/
+
+static rbc_event_callback g_event_callback;
+
+static bool g_is_initialized = false;
+
+
+
+/*****************************************************************************
+* Static Functions
+*****************************************************************************/
+
+
+
+
+
+/*****************************************************************************
+* Interface Functions
+*****************************************************************************/
+
+uint32_t rbc_init(const rbc_event_callback event_callback_function)
+{
+    if (g_is_initialized)
+    {
+        return NRF_ERROR_INVALID_STATE;
+    }
+    else
+    {
+        g_is_initialized = true;
+    }
+    
+    g_event_callback = event_callback_function;
+    
+    db_init();
+    
+    return NRF_SUCCESS;
+}
+
+
+uint32_t rbc_value_alloc(const rbc_value_handle_t value_handle, const bool is_volatile)
+{
+    if (!g_is_initialized)
+    {
+        return NRF_ERROR_INVALID_STATE;
+    }
+    
+    db_value_t* val = db_value_alloc();
+    
+    if (val == NULL)
+    {
+        return NRF_ERROR_NO_MEM;
+    }
+    
+    val->identifier.broadcast.id = value_handle;
+    
+    val->flags |= DB_VALUE_FLAG_IS_BROADCAST_POS;
+    
+    if (is_volatile)
+    {
+        val->flags |= DB_VALUE_FLAG_VOLATILE_POS;
+    }
+    
+    return NRF_SUCCESS;
+}
+
+
+uint32_t rbc_value_data_set(const rbc_value_handle_t value_handle, 
+    const uint8_t* data, 
+    const uint8_t len)
+{
+    if (!g_is_initialized)
+    {
+        return NRF_ERROR_INVALID_STATE;
+    }
+    
+    if (len > DB_VARIATION_MAX_LENGTH)
+    {
+        return NRF_ERROR_INVALID_LENGTH;
+    }
+    
+    db_value_t* val = db_value_get(value_handle, 0);
+    
+    if (val == NULL)
+    {
+        return NRF_ERROR_INVALID_ADDR;
+    }
+    
+    variation_t new_variation;
+    
+    memcpy(new_variation.data, data, len);
+    
+    new_variation.length = len;
+    new_variation.version = val->variation.version + 1;
+    
+    db_value_update_variation(val, &new_variation);
+    
+    
+    return NRF_SUCCESS;
+}
+
+
+uint32_t rbc_value_data_get(const rbc_value_handle_t value_handle, 
+    uint8_t* data, 
+    uint8_t* len)
+{
+    if (!g_is_initialized)
+    {
+        return NRF_ERROR_INVALID_STATE;
+    }
+    /*TODO*/
+    return NRF_SUCCESS;
+}
+
+
+uint32_t rbc_value_delete(const rbc_value_handle_t value_handle)
+{
+    if (!g_is_initialized)
+    {
+        return NRF_ERROR_INVALID_STATE;
+    }
+    /*TODO*/
+    return NRF_SUCCESS;
+}
+
+
+uint32_t rbc_available_slots(uint16_t* available)
+{
+    if (!g_is_initialized)
+    {
+        return NRF_ERROR_INVALID_STATE;
+    }
+    /*TODO*/
+    return NRF_SUCCESS;
+}
