328015982fad602f78e77305998ee47fbfb566e6
Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>
2015-08-13 17:48:04 +0200

    more organized debug pins

--- nRF51/rbc_mesh/include/rbc_mesh_common.h
+++ nRF51/rbc_mesh/include/rbc_mesh_common.h
@@ -1,4 +1,4 @@
-/***********************************************************************************
+/* copyright
 Copyright (c) Nordic Semiconductor ASA
 All rights reserved.
 
@@ -31,7 +31,7 @@ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
 ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-************************************************************************************/
+*/
 
 #ifndef _RBC_MESH_COMMON_H__
 #define _RBC_MESH_COMMON_H__
@@ -64,13 +64,21 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #endif
 
 #define PIN_MESH_TX         (0)
-#define PIN_SEARCHING       (1)
-#define PIN_CPU_IN_USE      (2)
+#define PIN_SD_EVT_HANDLER  (1)
+#define PIN_IN_TS           (2)
 #define PIN_CONSISTENT      (3)
 #define PIN_INCONSISTENT    (4)
 #define PIN_RX              (5)
-#define PIN_BUTTON          (6)
-#define PIN_ABORTED         (7)
+#define PIN_SWI0            (6)
+#define PIN_IN_CB           (7)
+
+#define PIN_EXTENSION_OK    (12)
+#define PIN_EXTENSION_FAIL  (13)
+#define PIN_RADIO_SIGNAL    (14)
+#define PIN_TIMER_SIGNAL    (15)
+
+#define PIN_TC_QUEUE_FULL   (19)
+
 
 #define PIN_INT0            (25)
 #define PIN_INT1            (26)
@@ -78,15 +86,12 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define PIN_TX1             (28)
 #define PIN_SYNC_TIME       (29)
 
-#define PIN_RADIO_SIGNAL    (3)
-#define PIN_TIMER_SIGNAL    (4)
-#define PIN_IN_TIMESLOT     (6)
 
 #define DEBUG_RADIO         (0)
 
-#define PIN_RADIO_STATE_RX  (1)
-#define PIN_RADIO_STATE_TX  (2)
-#define PIN_RADIO_STATE_IDLE (3)
+#define PIN_RADIO_STATE_RX  (16)
+#define PIN_RADIO_STATE_TX  (17)
+#define PIN_RADIO_STATE_IDLE (18)
 
 #if DEBUG_RADIO
     #define DEBUG_RADIO_SET_PIN(x) NRF_GPIO->OUTSET = (1 << (x))
