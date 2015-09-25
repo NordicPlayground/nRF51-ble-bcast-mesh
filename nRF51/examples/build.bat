c6308fb36e2023a2cabc53292e565b24a3d41a1b
Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>
2015-09-25 12:23:50 +0200

    build.bat: safer error handling

--- nRF51/examples/build.bat
+++ nRF51/examples/build.bat
@@ -1,19 +1,23 @@
 @echo off
+set currdir=%cd%
 for /D %%D in ("*") DO (
     echo Entering %%D
     cd %%D\arm
     for %%P in ("*.uvpro*") DO (
         echo Building %%P
+        UV4 -c %%P -j0
         UV4 -b %%P -z -j0 -o log.txt
         if ERRORLEVEL 2 (
             echo ERROR building %%P:
             type log.txt
-            cd ..\..
-            goto :eof
+            goto :cleanup
         )
         echo %%P built successfully.
     )
-    cd ..\..
+    cd %currdir%
 )
 echo ----------------------
 echo All builds successful!
+
+:cleanup
+cd %currdir%
