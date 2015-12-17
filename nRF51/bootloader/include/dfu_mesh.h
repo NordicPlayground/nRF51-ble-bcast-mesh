4b76f5a627bbe919c1c8df67c47c9a7a072e3b2e
Trond Einar Snekvik <trond.einar.snekvik@nordicsemi.no>
2015-12-17 21:40:30 +0100

    dfu_mesh: add missing function to header

--- nRF51/bootloader/include/dfu_mesh.h
+++ nRF51/bootloader/include/dfu_mesh.h
@@ -8,6 +8,7 @@ void dfu_init(void);
 uint32_t dfu_start(uint32_t* p_start_addr, uint32_t* p_bank_addr, uint32_t size, bool final_transfer);
 uint32_t dfu_data(uint32_t p_addr, uint8_t* p_data, uint16_t length);
 bool dfu_has_entry(uint32_t* p_addr, uint8_t* p_out_buffer, uint16_t len);
+bool dfu_get_oldest_missing_entry(uint32_t* p_start_addr, uint32_t** pp_entry, uint32_t* p_len);
 void dfu_sha256(uint8_t* p_hash);
 void dfu_end(void);
 
