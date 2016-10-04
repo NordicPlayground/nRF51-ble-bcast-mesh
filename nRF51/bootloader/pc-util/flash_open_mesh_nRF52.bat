23b05c6c89908ea6942b1774344bdd3d9b58f43d
baruaanomadarshishuvro <Anomadarshi.Barua@nordicsemi.no>
2016-10-04 14:18:40 +0200

    flash_open_mesh.bat for nRF52 (powershell)

--- nRF51/bootloader/pc-util/flash_open_mesh_nRF52.bat
+++ nRF51/bootloader/pc-util/flash_open_mesh_nRF52.bat
@@ -12,9 +12,9 @@ cd %bootloaderfolder%
 REM  nrfjprog --program .\bin\bootloader_serial_xxAC_new_3.hex
 nrfjprog --program "C:\Users\anba\nordic sdk\nRF5_SDK_12.0.0_12f24da\examples\nRF51-ble-broadcast-mesh-private\nRF51\bootloader\arm\_build_serial\bootloader_serial_xxAA.hex" --family NRF52 
 del .\pc-util\example52.hex
-.\pc-util\device_page .\pc-util\example52
+.\pc-util\device_page.exe .\pc-util\example52 --nrf52
 nrfjprog --program .\pc-util\example52.hex --family NRF52  
 REM nrfjprog --program .\test\rbc_gateway_example_serial_trsn_21_sep_16_2.hex
-nrfjprog --program "C:\Users\anba\nordic sdk\nRF5_SDK_12.0.0_12f24da\examples\nRF51-ble-broadcast-mesh-private\nRF51\examples\BLE_Gateway\arm\_build\rbc_gateway_example_52.hex"
+nrfjprog --program "C:\Users\anba\nordic sdk\nRF5_SDK_12.0.0_12f24da\examples\nRF51-ble-broadcast-mesh-private\nRF51\examples\BLE_Gateway\arm\_build\rbc_gateway_example_52.hex" --family NRF52 
 nrfjprog --reset --family NRF52  
 C:\Users\anba\Desktop\pc-nrfutil\nrfutil.exe dfu genpkg --application test/nrf52832_xxaa.hex --company-id 0x00000059 --application-id 1 --application-version 2 --sd-req 0x0084 --mesh dfu_test.zip
