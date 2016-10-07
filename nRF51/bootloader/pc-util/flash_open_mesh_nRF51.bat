0faed043d412860bca3e436fb74c26a110d7de8e
baruaanomadarshishuvro <Anomadarshi.Barua@nordicsemi.no>
2016-10-07 19:44:47 +0200

    Batch file update

--- nRF51/bootloader/pc-util/flash_open_mesh_nRF51.bat
+++ nRF51/bootloader/pc-util/flash_open_mesh_nRF51.bat
@@ -1,20 +1,38 @@
 SETLOCAL
-REM @echo off
+REM @echo on
+
 REM parameter 1 = nRF51/bootloader folder
 SET bootloaderfolder=%~f1
+
 REM parameter 2 = folder with the S110 v8.0.0 softdevice
 SET softdevicefolder=%~f2
-nrfjprog --eraseall --family NRF51
+
+REM Parameter 3 = Segger ID of the gateway nRF5x board
+SET seggerid=%3
+
+nrfjprog -s %seggerid% --eraseall --family NRF51
+
 cd %softdevicefolder%
-REM nrfjprog --program s111_nrf51_8.0.0_softdevice.hex
-nrfjprog --program s110_nrf51_8.0.0_softdevice.hex --family NRF51
+
+nrfjprog -s %seggerid% --program s110_nrf51_8.0.0_softdevice.hex --family NRF51 
+
 cd %bootloaderfolder%
-REM  nrfjprog --program .\bin\bootloader_serial_xxAC_new_3.hex
-nrfjprog --program "C:\Users\anba\nordic sdk\nRF51_SDK_8.1.0_b6ed55f\examples\nRF51-ble-broadcast-mesh-private\nRF51\bootloader\arm\_build_serial\bootloader_serial_xxAC.hex" --family NRF51
+
+nrfjprog -s %seggerid% --program .\bin\bootloader_serial_xxAC.hex --family NRF51 
+
+
 del .\pc-util\example.hex
-.\pc-util\device_page .\pc-util\example
-nrfjprog --program .\pc-util\example.hex --family NRF51
-REM nrfjprog --program .\test\rbc_gateway_example_serial_trsn_21_sep_16_2.hex
-nrfjprog --program "C:\Users\anba\nordic sdk\nRF51_SDK_8.1.0_b6ed55f\examples\nRF51-ble-broadcast-mesh-private\nRF51\examples\BLE_Gateway\arm\_build\rbc_gateway_example_serial.hex"
-nrfjprog --reset --family NRF51
-C:\Users\anba\Desktop\pc-nrfutil\nrfutil.exe dfu genpkg --application test/nrf51422_xxac_PCA10031_S110_Blinky.hex --company-id 0x00000059 --application-id 1 --application-version 2 --sd-req 0x0064 --mesh dfu_test.zip
+.\pc-util\device_page .\pc-util\example --nrf51
+nrfjprog -s %seggerid% --program .\pc-util\example.hex --family NRF51
+
+
+nrfjprog -s %seggerid% --program ..\examples\BLE_Gateway\bin\rbc_gateway_example_serial.hex --family NRF51
+
+
+..\..\..\pc-nrfutil\nrfutil.exe dfu genpkg --application test/nrf51422_xxac_PCA10031_S110_Blinky.hex --company-id 0x00000059 --application-id 1 --application-version 2 --sd-req 0x0064 --mesh dfu_test.zip
+
+
+
+REM Reset the nRF5x to get it ready for DFU
+nrfjprog -s %seggerid% --reset --family NRF51
+
--- nRF51/bootloader/pc-util/for_loop_batch_nRF51.bat
+++ nRF51/bootloader/pc-util/for_loop_batch_nRF51.bat
@@ -6,6 +6,6 @@ REM parameter 2 = folder with the S110 v8.0.0 softdevice
 
 
 nrfjprog -i > .\tmp_remove_me.txt
-FOR /f %%i IN (.\tmp_remove_me.txt) DO "%~dp0/flash_open_mesh_nRF51_v2.bat" "%~f1" "%~f2" %%i
+FOR /f %%i IN (.\tmp_remove_me.txt) DO "%~dp0/flash_open_mesh_nRF51.bat" "%~f1" "%~f2" %%i
 
 del .\tmp_remove_me.txt
