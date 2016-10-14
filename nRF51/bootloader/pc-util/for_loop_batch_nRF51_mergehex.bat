a0466ded7d7b550d45e2fd85ac96f4028710d3be
baruaanomadarshishuvro <Anomadarshi.Barua@nordicsemi.no>
2016-10-14 12:35:48 +0200

    script for mergehex

---
+++ nRF51/bootloader/pc-util/for_loop_batch_nRF51_mergehex.bat
@@ -0,0 +1,11 @@
+SETLOCAL
+ECHO ON
+
+REM parameter 1 = nRF51/bootloader folder
+REM parameter 2 = folder with the S110 v8.0.0 softdevice
+
+
+nrfjprog -i > .\tmp_remove_me.txt
+FOR /f %%i IN (.\tmp_remove_me.txt) DO "%~dp0/flash_open_mesh_nRF51_mergehex.bat" "%~f1" "%~f2" %%i
+
+del .\tmp_remove_me.txt
