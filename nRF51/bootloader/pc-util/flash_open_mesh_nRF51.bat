f63dbb3ef56b531075e41e67b8813bf66c41ca76
baruaanomadarshishuvro <Anomadarshi.Barua@nordicsemi.no>
2016-10-14 15:29:03 +0200

    mergehex and test folder is deleted

--- nRF51/bootloader/pc-util/flash_open_mesh_nRF51.bat
+++ nRF51/bootloader/pc-util/flash_open_mesh_nRF51.bat
@@ -19,18 +19,17 @@ nrfjprog -s %seggerid% --program s110_nrf51_8.0.0_softdevice.hex --family NRF51
 cd %bootloaderfolder%
 
 nrfjprog -s %seggerid% --program .\bin\bootloader_serial_xxAC.hex --family NRF51 
-REM nrfjprog -s %seggerid% --program "C:\Users\anba\nordic sdk\nRF51_SDK_8.1.0_b6ed55f\examples\nRF51-ble-broadcast-mesh-private\nRF51\bootloader\arm\_build_serial\bootloader_serial_xxAC.hex" --family NRF51
 
 del .\pc-util\example.hex
 .\pc-util\device_page .\pc-util\example --nrf51
 nrfjprog -s %seggerid% --program .\pc-util\example.hex --family NRF51
 
 
-nrfjprog -s %seggerid% --program ..\examples\BLE_Gateway\bin\rbc_gateway_example_serial.hex --family NRF51
+nrfjprog -s %seggerid% --program ..\examples\BLE_Gateway\bin\rbc_gateway_example_serial_nRF51422_xxAC.hex --family NRF51
 
 
 
-..\..\..\pc-nrfutil\nrfutil.exe dfu genpkg --application test\nrf51422_xxac_PCA10031_S110_Blinky.hex --company-id 0x00000059 --application-id 1 --application-version 2 --sd-req 0x0064 --mesh dfu_test.zip
+nrfutil.exe dfu genpkg --application ..\examples\BLE_Gateway\bin\rbc_gateway_example_serial_blinky_PCA10031_nRF51422_xxAC.hex --company-id 0x00000059 --application-id 1 --application-version 2 --sd-req 0x0064 --mesh dfu_test.zip
 
 
 
