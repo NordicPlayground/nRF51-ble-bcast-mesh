4d9b7de99e7a36b18a151a5acf4fb89309a709b4
Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>
2016-10-17 16:24:58 +0200

    Fix signed banked transfers not being accepted

--- nRF51/bootloader/core/include/bootloader_info.h
+++ nRF51/bootloader/core/include/bootloader_info.h
@@ -35,7 +35,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "uECC.h"
 #include "bl_if.h"
 
-#define INFO_ENTRY_MAX_LEN  (64)
+#define INFO_ENTRY_MAX_LEN  (0x54)
 
 typedef struct
 {
