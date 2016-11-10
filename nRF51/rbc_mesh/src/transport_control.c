6b2ce753cd956c9f836cd41f77bf3a7e9e55261f
baruaanomadarshishuvro <Anomadarshi.Barua@nordicsemi.no>
2016-11-10 16:31:32 +0100

    #ifdef issues related with testbench

--- nRF51/rbc_mesh/src/transport_control.c
+++ nRF51/rbc_mesh/src/transport_control.c
@@ -29,9 +29,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ************************************************************************************/
 
 #include <string.h>
-
 #include "transport_control.h"
-
 #include "radio_control.h"
 #include "mesh_gatt.h"
 #include "mesh_packet.h"
@@ -44,6 +42,21 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "version_handler.h"
 #include "mesh_aci.h"
 #include "app_error.h"
+
+#if defined(WITH_ACK_MASTER) || defined (WITHOUT_ACK_MASTER)|| defined (WITH_ACK_SLAVE)
+
+#include "SEGGER_RTT.h"
+#include "nrf_gpio.h"
+#include "boards.h"
+#include <stdio.h>
+
+#endif
+
+#if defined(WITH_ACK_SLAVE)
+#include <handle.h>
+#endif
+
+
 
 #ifdef MESH_DFU
 #include "dfu_types_mesh.h"
@@ -70,12 +83,45 @@ static rbc_mesh_packet_peek_cb_t mp_packet_peek_cb;
 
 /* STATS */
 #ifdef PACKET_STATS
+
+
+#if defined (WITH_ACK_MASTER) 
+static struct
+{
+    uint32_t queue_drop;
+    uint32_t queue_ok;
+    uint32_t crc_fail;
+} m_packet_stats __attribute__((at(0x20003030)))  = {0};
+
+
+#elif defined(WITHOUT_ACK_MASTER) 
+static struct
+{
+    uint32_t queue_drop;
+    uint32_t queue_ok;
+    uint32_t crc_fail;
+} m_packet_stats __attribute__((at(0x2000273C))) = {0};
+
+
+
+#elif defined(WITH_ACK_SLAVE)
+static struct
+{
+    uint32_t queue_drop;
+    uint32_t queue_ok;
+    uint32_t crc_fail;
+} m_packet_stats __attribute__((at(0x20002750))) = {0};
+
+#else
 static struct
 {
     uint32_t queue_drop;
     uint32_t queue_ok;
     uint32_t crc_fail;
 } m_packet_stats = {0};
+
+#endif
+
 #endif
 /******************************************************************************
 * Static functions
@@ -102,6 +148,7 @@ static void order_search(void)
     }
 }
 
+#ifndef WITH_ACK_SLAVE
 /* immediate radio callback, executed in STACK_LOW */
 static void rx_cb(uint8_t* p_data, bool success, uint32_t crc, uint8_t rssi)
 {
@@ -139,6 +186,56 @@ static void rx_cb(uint8_t* p_data, bool success, uint32_t crc, uint8_t rssi)
     /* no longer needed in this context */
     mesh_packet_ref_count_dec((mesh_packet_t*) p_data);
 }
+
+#endif
+
+
+#if defined(WITH_ACK_SLAVE)
+/* immediate radio callback, executed in STACK_LOW */
+static void rx_cb(uint8_t* p_data, bool success, uint32_t crc, uint8_t rssi)
+{
+	mesh_packet_t* p_mesh_packet = (mesh_packet_t*) p_data;
+    mesh_adv_data_t* p_adv_data = (mesh_adv_data_t*) p_mesh_packet->payload;
+	  
+	if (success &&  (p_adv_data->handle == node_handle) &&((mesh_packet_t*) p_data)->header.length <= MESH_PACKET_BLE_OVERHEAD + BLE_ADV_PACKET_PAYLOAD_MAX_LENGTH)
+     {
+        async_event_t evt;
+        evt.type = EVENT_TYPE_PACKET;
+        evt.callback.packet.payload = p_data;
+        evt.callback.packet.crc = crc;
+        evt.callback.packet.timestamp = timer_now();
+        evt.callback.packet.rssi = rssi;
+        if (event_handler_push(&evt) != NRF_SUCCESS)
+        {
+            m_state.queue_saturation = true;
+#ifdef PACKET_STATS
+            m_packet_stats.queue_drop++;
+#endif
+        }
+        else
+        {
+            mesh_packet_ref_count_inc((mesh_packet_t*) p_data); /* event handler has a ref */
+
+#ifdef PACKET_STATS
+            m_packet_stats.queue_ok++;
+#endif
+        }
+    }
+    else if (crc < 0x1000000) /* don't want to trigger on artifical crc values */
+    {
+#ifdef PACKET_STATS
+        m_packet_stats.crc_fail++;
+#endif
+    }
+
+    /* no longer needed in this context */
+    mesh_packet_ref_count_dec((mesh_packet_t*) p_data);
+}
+
+#endif
+
+
+
 
 /* radio tx cb, executed in APP_LOW */
 static void async_tx_cb(void* p_context)
